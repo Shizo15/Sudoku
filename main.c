896f60c3889a7fd2cf04426b32b5f4abdac93bc4
Shizo15 <damianszymczyk153@gmail.com>
2025-04-27 16:20:26 +0200

    fix: created todo's and started to adapt the sudoku implementation

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -62,24 +63,40 @@ int selectDifficulty()
 void printInstructions()
 {
     printf("\nInstructions:\n");
+    //TODO
+    // Napisać jakieś podstawowe instrukcje jak obsugiwać grę
 
 }
 void startNewGame()
 {
+    selectBoardSize();
+    selectDifficulty();
+    //TODO
+    // Zrobić mechanizm inicjalizacji planszy do sudoku o podanym wymiarze
+    // potem następną fukncją wypełnić planszę liczbami zgodnie z rozmiarem i wybranym poziomem trudności
+
+    printf("\nStarting new game.\n");
 
 }
 void saveGame()
 {
     printf("\nGame saved successfully!\n");
+    //TODO
+    // pomyśleć w jaki sposób zapisywać progress aktualnej rozgrywki do pliku
+    // dodać też możlowść save'a do menu rozgrywki??
 
 }
 void loadGame()
 {
     printf("\nGame loaded successfully!\n");
-
+    //TODO
+    // tu trzeba zrobić wyświetlanie dostępnych plików z savem w katalogu gry i możliwośc wyboru save'a
+    //
 }
 
 // inne funkcje
+//TODO
+// zroibć logikę gry i pomyśleć jak obsługiwać ruchy gracza
 
 void printMainMenu()
 {
@@ -116,7 +133,7 @@ void printMainMenu()
 //PRZEPISANE SUDOKU ZE STRONY
 
 // Returns false if given 3x3 block contains num
-bool unUsedInBox(int grid[9][9], int rowStart, int colStart, int num) {
+bool isSafeInBox(int grid[9][9], int rowStart, int colStart, int num) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (grid[rowStart + i][colStart + j] == num) {
@@ -126,6 +143,8 @@ bool unUsedInBox(int grid[9][9], int rowStart, int colStart, int num) {
     }
     return true;
 }
+//todo
+// tutaj przerobić na generowanie wartości zależnie od wybranej wielkości
 
 // Fill a 3x3 matrix
 void fillBox(int grid[9][9], int row, int col) {
@@ -134,14 +153,16 @@ void fillBox(int grid[9][9], int row, int col) {
         for (int j = 0; j < 3; j++) {
             do {
                 num = (rand() % 9) + 1;
-            } while (!unUsedInBox(grid, row, col, num));
+            } while (!isSafeInBox(grid, row, col, num));
             grid[row + i][col + j] = num;
         }
     }
 }
+//todo
+// tutaj przerobić na sprawdzanie wartości zależnie od wybranej wielkości
 
 // Check if it's safe to put num in row i
-bool unUsedInRow(int grid[9][9], int i, int num) {
+bool isSafeInRow(int grid[9][9], int i, int num) {
     for (int j = 0; j < 9; j++) {
         if (grid[i][j] == num) {
             return false;
@@ -149,9 +170,11 @@ bool unUsedInRow(int grid[9][9], int i, int num) {
     }
     return true;
 }
+//todo
+// tutaj przerobić na sprawdzanie wartości zależnie od wybranej wielkości
 
 // Check if it's safe to put num in column j
-bool unUsedInCol(int grid[9][9], int j, int num) {
+bool isSafeInCol(int grid[9][9], int j, int num) {
     for (int i = 0; i < 9; i++) {
         if (grid[i][j] == num) {
             return false;
@@ -159,24 +182,36 @@ bool unUsedInCol(int grid[9][9], int j, int num) {
     }
     return true;
 }
+//todo
+// tutaj przerobić na sprawdzanie wartości zależnie od wybranej wielkości
 
 // Check if it's safe to put num in the cell (i, j)
-bool checkIfSafe(int grid[9][9], int i, int j, int num) {
-    return (unUsedInRow(grid, i, num) &&
-           (unUsedInCol(grid, j, num) &&
-           unUsedInBox(grid, i - i % 3, j - j % 3, num)));
+bool checkIfSafe(int n, int i, int j, int num) {
+    int square = (int)sqrt(n);
+    int grid[n][n];
+    return (isSafeInRow(grid, i, num) &&
+           (isSafeInCol(grid, j, num) &&
+           isSafeInBox(grid, i - i % square, j - j % square, num)));
 }
+//todo
+// tutaj przerobić na generowanie wartości zależnie od wybranej wielkości
 
 // Fill the diagonal 3x3 matrices
-void fillDiagonal(int grid[9][9]) {
-    for (int i = 0; i < 9; i += 3) {
+void fillDiagonal(int n) {
+    int grid[n][n];
+    int square = (int)sqrt(n);
+
+    for (int i = 0; i < n; i += square) {
         fillBox(grid, i, i);
     }
 }
+//todo
+// tutaj przerobić na generowanie wartości zależnie od wybranej wielkości
 
 // Fill remaining blocks in the grid
-bool fillRemaining(int grid[9][9], int i, int j) {
-    if (i == 9) {
+bool fillRemaining(int n, int i, int j) {
+    int grid[n][n];
+    if (i == n) {
         return true;
     }
 
@@ -188,7 +223,7 @@ bool fillRemaining(int grid[9][9], int i, int j) {
         return fillRemaining(grid, i, j + 1);
     }
 
-    for (int num = 1; num <= 9; num++) {
+    for (int num = 1; num <= n; num++) {
         if (checkIfSafe(grid, i, j, num)) {
             grid[i][j] = num;
             if (fillRemaining(grid, i, j + 1)) {
@@ -200,13 +235,17 @@ bool fillRemaining(int grid[9][9], int i, int j) {
 
     return false;
 }
+//todo
+// tutaj przerobić na generowanie wartości zależnie od wybranej wielkości
+// dodać usuwanie pól zaleźnie od poziomu trudności
 
 // Remove K digits randomly from the grid
-void removeKDigits(int grid[9][9], int k) {
+void removeKDigits(int n, int k) {
+    int grid [n][n];
     while (k > 0) {
-        int cellId = rand() % 81;
-        int i = cellId / 9;
-        int j = cellId % 9;
+        int cellId = rand() % (n*n);
+        int i = cellId / n;
+        int j = cellId % n;
 
         if (grid[i][j] != 0) {
             grid[i][j] = 0;
@@ -214,12 +253,15 @@ void removeKDigits(int grid[9][9], int k) {
         }
     }
 }
+//todo
+// tutaj przerobić na generowanie wartości zależnie od wybranej wielkości
 
 // Generate a Sudoku grid with K empty cells
-void sudokuGenerator(int grid[9][9], int k) {
+void sudokuGenerator(int n, int k) {
     // Initialize grid with 0
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
+    int grid[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             grid[i][j] = 0;
         }
     }
@@ -234,17 +276,24 @@ int main(void)
 {
     //poziomy trurdności to ilość wygenerowanych liczb na nowej planszy(podpowiedzi)
 
-    // zrobić menu główne i poboczne (do gry) z którego będzie można wykonywać ruchy i prześć do menu głównego
-    // printMainMenu();
+    // zrobić menu główne i poboczne (do gry) z którego będzie można wykonywać ruchy i przejść do menu głównego
+    // jak obliczać liczbę podpowiedzi względem rozmiaru planszy i poziomu trudddności?
+    // zakres liczb zależy od wielkości planszy
+
+    printMainMenu();
 
 
     //PRZEPISANE SUDOKU
-
+    //todo
+    // to trzeba przenieść do metody startującej gre
     srand(time(0));
-    int k = 20;
+    int k = 30;
     int sudoku[9][9];
 
     sudokuGenerator(sudoku, k);
+    //
+    //todo
+    // to trzeba będzie prtinować i aktualizować w każdej iteracji gry / po każdej akcji
 
     // Print the generated Sudoku puzzle
     for (int i = 0; i < 9; i++) {
@@ -254,5 +303,7 @@ int main(void)
         }
         printf("\n");
     }
+    //todo
+    // dodać memiki oczywiście do readme
     return 0;
 }
